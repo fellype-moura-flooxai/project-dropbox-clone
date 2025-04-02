1cbc3da645c7e783f0e9106db50e3a5e3adcb2e9
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-02 10:34:08 -0300

    feat: deleting firebase directory

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -48,6 +48,64 @@ class DropBoxController {
 
           }
 
+          removeFolderTask(ref, name){
+
+            return new Promise((resolve, reject) => {
+
+                let folderRef = this.getFirebaseRef(ref + '/' + name);
+
+                folderRef.on('value', snapshot => {
+
+                    snapshot.forEach(item => {
+
+                        let data = item.val();
+                        data.key = item.key;
+
+                        if (data.type === 'folder') {
+
+                            this.removeFolderTask(ref + '/' + name, data.name).then(()=>{
+
+                                resolve({
+                                    fields: {
+                                        key: data.key
+                                    }
+                                });
+        
+                            }).catch(err => {
+                                reject(err);
+                            });
+
+                        } else if(data.type) {
+
+                            this.removeFile(ref + '/' + name, data.name).then(()=>{
+
+                                resolve({
+                                    fields: {
+                                        key: data.key
+                                    }
+                                });
+        
+                            }).catch(err => {
+                                reject(err);
+                            });
+
+
+                        }
+
+
+                    });
+
+                    folderRef.remove();
+                    folderRef.off('value');
+
+                });
+
+            });
+
+          };
+
+
+
     removeTask(){
 
         let promises =[];
@@ -59,28 +117,35 @@ class DropBoxController {
 
             promises.push(new promises((resolve, reject) => {
 
-              let fileRef = firebase.storage().ref(this.currentFolder.join('/')) .child(file.name);
+                if (file.type === 'folder') {
 
-              fileRef.delete().then(()=>{
+                    this.removeFolderTask(this.currentFolder.join('/'), file.name).then(()=>{
 
-                resolve({
-                    fields:{
-                        key
-                    }
+                        resolve({
+                            fields:{
+                                key
+                            }
+                        });
+                    });
 
-                });
+                } else if (file.type) {
 
-              }).catch(err=>{
+                    this.removeFile(this.currentFolder.join('/'), file.name)
+                }
 
-                reject(err);
-
-              });
-
-            }));
-
+                }));
+                
         });
 
         return Promise.all(promises);
+
+    }
+
+    removeFile(ref, name){
+
+        let fileRef = firebase.storage().ref(ref) .child(name);
+
+              return fileRef.delete();
 
     }
 
