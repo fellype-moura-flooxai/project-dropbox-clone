79d1471c663dc4cf4de6c5fe9fca72715559cfc2
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-02 09:27:40 -0300

    feat: creating route to open the file

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/file', (req, res) => {
+
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)) {
+
+    fs.readFile(path, (err, data)=>{
+
+      if (err) {
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+
+      } else {
+
+        res.status(200).end(data);
+
+      }
+
+    });
+
+  } else {
+
+    res.status(404).json({
+      error:'File not found.'
+
+    });
+
+  }
+
+});
+
 router.delete('/file', (req, res)=>{
 
   let form = new formidable.IncomingForm({
@@ -38,7 +72,13 @@ router.delete('/file', (req, res)=>{
 
       }); 
 
-    }
+    } else {
+
+        res.status(404).json({
+          error:'File not found.'
+        });
+    
+      }
 
   });
 
